067cb7cd4841b6ee0073ead3d8988b5a21c6367a
VedhaSankar <vedhasankar26@gmail.com>
2022-03-22 14:35:11 +0530

    created a basic form

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+
+    name = request.values.get("name")
+    gender = request.values.get("gender")
+    age = request.values.get("age")
+    profession = request.values.get("profession")
+    review = request.values.get("review")
+
+    print (name, gender, age, profession, review)
+
+    return render_template('index.html')
+
+@app.route('/ping')
+def ping():
+
+    result = {
+        "ping"  : "pong"
+    }
+    return result
+
+
+if __name__== "__main__":
+    app.run(host="0.0.0.0", debug = True, port = 5003)
+
