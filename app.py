fd97a2861d5e1eb19f9cf18b54d6827c4ecd5ef3
VedhaSankar <vedhasankar26@gmail.com>
2022-03-26 22:49:13 +0530

    emotion pushed to db

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
 from model_maker import predict_emotion
+from pymongo import MongoClient
+import os
+
 
 app = Flask(__name__)
+
+MONGO_URI           = os.environ.get('MONGO_URI')
+
+client = MongoClient(MONGO_URI)  
+DB_NAME = 'movie_reviews_emotion'
+database = client[DB_NAME]
 
 @app.route('/', methods = ['GET', 'POST'])
 def start():
@@ -18,6 +27,17 @@ def start():
     if request.method == 'POST':
 
         emotion = predict_emotion(review)[0]
+
+        review_dict = {
+            'review'    : review,
+            'emotion'   : emotion
+        }
+
+        collection_name = 'review_details'
+
+        new_collection = database[collection_name]
+        x = new_collection.insert_one(review_dict)
+        print(x)
 
         return render_template('index.html', emotion = emotion)
 
